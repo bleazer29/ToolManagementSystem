8cbe45ea4de6a21e96649cdce20a81f92db94424
Максим Коле <maksim_kole@ia.ua>
2020-08-19 12:55:44 +0300

    Add project files.

---
+++ ToolManagementSystem.Shared/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToolManagementSystem.Shared
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
