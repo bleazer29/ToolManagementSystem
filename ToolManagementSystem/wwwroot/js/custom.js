e819e2a1cd2834b8e9b0fe83a356b4f966c9a478
evgeniya_osadchaya <jhenya1306@gmail.com>
2020-08-27 12:44:59 +0300

    roles added

--- ToolManagementSystem/wwwroot/js/custom.js
+++ ToolManagementSystem/wwwroot/js/custom.js
@@ -33,22 +33,22 @@ function AddMainMenuTree(elementName) {
             'data': [
                 { "id": "1", "parent": "#", "text": "НСИ", "state": { "opened": "true", "disabled": "true" } },
                 { "id": "2", "parent": "#", "text": "Склад", "state": { "opened": "true" }, "a_attr": { "href": "/Tools/" } },
-                { "id": "3", "parent": "#", "text": "Контракты", "state": { "opened": "true" }, "a_attr": { "href": "/NRI/Contracts" } },
+                { "id": "3", "parent": "#", "text": "Контракты", "state": { "opened": "true" }, "a_attr": { "href": "/Contracts" } },
                 { "id": "4", "parent": "#", "text": "Наряды на работу", "state": { "opened": "true" }, "a_attr": { "href": "/Orders/" } },
-                { "id": "5", "parent": "#", "text": "Сервисное обслуживание", "state": { "opened": "true" } },
+                { "id": "5", "parent": "#", "text": "Сервисное обслуживание", "state": { "opened": "true" },  },
                 { "id": "6", "parent": "#", "text": "Ремонт", "state": { "opened": "true" } },
                 { "id": "7", "parent": "#", "text": "Администрирование", "state": { "opened": "true", "disabled": "true" } },
                 { "id": "9", "parent": "#", "text": "Отчёты", "state": { "opened": "true" } },
                 { "id": "8", "parent": "#", "text": "Документация", "state": { "opened": "true" } },
                 { "id": "11", "parent": "19", "text": "Подразделения", "state": { "opened": "true" }, "a_attr": { "href": "/NRI/Departments" } },
                 { "id": "10", "parent": "20", "text": "Статусы", "state": { "opened": "true" }, "a_attr": { "href": "/NRI/Statuses" } },
-                { "id": "12", "parent": "20", "text": "Класификация", "state": { "opened": "true" }, "a_attr": { "href": "/NRI/Classification" } },
+                { "id": "12", "parent": "20", "text": "Классификация", "state": { "opened": "true" }, "a_attr": { "href": "/NRI/Classification" } },
                 { "id": "13", "parent": "20", "text": "Атрибуты", "state": { "opened": "true" }, "a_attr": { "href": "/NRI/Attributes" } },
                 { "id": "14", "parent": "20", "text": "Номенклатура", "state": { "opened": "true" }, "a_attr": { "href": "/NRI/Nomenclature" } },
                 { "id": "15", "parent": "21", "text": "Контрагенты", "state": { "opened": "true" }, "a_attr": { "href": "/NRI/Counterparties" } },
                 { "id": "16", "parent": "21", "text": "Скважины", "state": { "opened": "true" }, "a_attr": { "href": "/NRI/Wells" } },
-                { "id": "17", "parent": "7", "text": "Пользователи", "state": { "opened": "true" }, "a_attr": { "href": "/NRI/Users" } },
-                { "id": "18", "parent": "7", "text": "Роли", "state": { "opened": "true" }, "a_attr": { "href": "/NRI/Roles" } },
+                { "id": "17", "parent": "7", "text": "Пользователи", "state": { "opened": "true" }, "a_attr": { "href": "/Administrative/Users" } },
+                { "id": "18", "parent": "7", "text": "Роли", "state": { "opened": "true" }, "a_attr": { "href": "/Administrative/Roles" } },
                 { "id": "19", "parent": "1", "text": "Компания", "state": { "opened": "true", "disabled": "true" } },
                 { "id": "20", "parent": "1", "text": "Инструменты", "state": { "opened": "true", "disabled": "true" } },
                 { "id": "21", "parent": "1", "text": "Клиенты", "state": { "opened": "true", "disabled": "true" } }
@@ -87,13 +87,13 @@ function AddClasificationTree(elementName) {
 
     // tree data
     var data;
-    data = [{ "id": "1", "parent": "#", "text": "Вид1", "data": { "nodeId": "1" } },
-        { "id": "2", "parent": "#", "text": "Вид2", "data": { "nodeId": "2" } },
-        { "id": "3", "parent": "#", "text": "Вид3", "data": { "nodeId": "3" } },
-        { "id": "4", "parent": "1", "text": "Тип1", "data": { "nodeId": "4" } },
-        { "id": "5", "parent": "1", "text": "Тип2", "data": { "nodeId": "5" } },
-        { "id": "6", "parent": "2", "text": "Тип3", "data": { "nodeId": "6" } },
-        { "id": "7", "parent": "4", "text": "Тип4", "data": { "nodeId": "7" } }];
+    data = [{ "id": "1", "parent": "#", "text": "Вид1", "data": { "nodeId": "1" }, "state": { "opened": "true"}},
+        { "id": "2", "parent": "#", "text": "Вид2", "data": { "nodeId": "2" }, "state": { "opened": "true"} },
+        { "id": "3", "parent": "#", "text": "Вид3", "data": { "nodeId": "3" }, "state": { "opened": "true"} },
+        { "id": "4", "parent": "1", "text": "Тип1", "data": { "nodeId": "4" }, "state": { "opened": "true"} },
+        { "id": "5", "parent": "1", "text": "Тип2", "data": { "nodeId": "5" }, "state": { "opened": "true"} },
+        { "id": "6", "parent": "2", "text": "Тип3", "data": { "nodeId": "6" }, "state": { "opened": "true"} },
+        { "id": "7", "parent": "4", "text": "Тип4", "data": { "nodeId": "7" }, "state": { "opened": "true"} }];
 
     $(elem).jstree({
         "plugins": ["grid", "wholerow", "search"],
