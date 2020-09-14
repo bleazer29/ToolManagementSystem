5fd4c9cb6a40886691ef52fad9805c55139973f6
Максим Коле <maksim_kole@ia.ua>
2020-09-14 14:12:16 +0300

    Client WorkOrderManager Added

---
+++ ToolManagementSystem.Client/Managers/Maintenances/WorkOrderManager.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ToolManagementSystem.Shared.Models;
+
+namespace ToolManagementSystem.Client.Managers.Maintenances
+{
+    public class WorkOrderManager
+    {
+        static string apiControllerName { get; set; } = "WorkOrders";
+        public async static Task<List<WorkOrder>> GetWorkOrdersAsync(string filterByName, DateTime filterByDateStart, DateTime filterByDateEnd,
+            string filterByResponsibleName, string sortField, string filterByStatus, bool isAscendingSort)
+        {
+            List<WorkOrder> workOrders = null;
+            try
+            {
+                string requestAddress = "/" + filterByDateStart.ToString("yyyy-MM-dd") + "/" + filterByDateEnd.ToString("yyyy-MM-dd") + "?name="
+                    + filterByName + "&responsible=" + filterByResponsibleName + "&status=" + filterByStatus;
+                HttpResponseMessage response = await CustomHttpClient.GetClientInstance().GetAsync(apiControllerName + requestAddress);
+                if (response.IsSuccessStatusCode)
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    workOrders = JsonConvert.DeserializeObject<List<WorkOrder>>(apiResponse);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            workOrders = SortWorkOrders(workOrders, sortField, isAscendingSort);
+            return workOrders;
+        }
+
+        public static List<WorkOrder> SortWorkOrders(List<WorkOrder> workOrders, string sortField, bool isAscendingSort)
+        {
+            if (isAscendingSort)
+            {
+                if (sortField == "Name")
+                {
+                    workOrders = workOrders.OrderBy(x => x.Name).ToList();
+                }
+                else if (sortField == "DateStart")
+                {
+                    workOrders = workOrders.OrderBy(x => x.StartDate).ToList();
+                }
+                else if (sortField == "DateEnd")
+                {
+                    workOrders = workOrders.OrderBy(x => x.EndDate).ToList();
+                }
+                else if (sortField == "EstimateDateEnd")
+                {
+                    workOrders = workOrders.OrderBy(x => x.EstimatedEndDate).ToList();
+                }
+                else if (sortField == "Responsible")
+                {
+                    workOrders = workOrders.OrderBy(x => x.ResponsibleUser == null ? "" : x.ResponsibleUser.Fio).ToList();
+                }
+            }
+            else
+            {
+                if (sortField == "Name")
+                {
+                    workOrders = workOrders.OrderByDescending(x => x.Name).ToList();
+                }
+                else if (sortField == "DateStart")
+                {
+                    workOrders = workOrders.OrderByDescending(x => x.StartDate).ToList();
+                }
+                else if (sortField == "DateEnd")
+                {
+                    workOrders = workOrders.OrderByDescending(x => x.EndDate).ToList();
+                }
+                else if (sortField == "EstimateDateEnd")
+                {
+                    workOrders = workOrders.OrderByDescending(x => x.EstimatedEndDate).ToList();
+                }
+                else if (sortField == "Counterparty")
+                {
+                    workOrders = workOrders.OrderByDescending(x => x.ResponsibleUser == null ? "" : x.ResponsibleUser.Fio).ToList();
+                }
+            }
+            return workOrders;
+        }
+
+    }
+}
--- ToolManagementSystem.Client/Pages/Maintenances/Maintenance.cshtml.cs
+++ ToolManagementSystem.Client/Pages/Maintenances/Maintenance.cshtml.cs
@@ -4,14 +4,57 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToolManagementSystem.Shared.Models;
+using ToolManagementSystem.API;
+using ToolManagementSystem.Client;
+using ToolManagementSystem.Client.Managers.NRI;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net;
+using ToolManagementSystem.Client.Managers.Maintenances;
 
 namespace ToolManagementSystem.Client.Pages.Maintenances
 {
     public class MaintenanceModel : PageModel
     {
-        public void OnGet()
-        {
+        public List<WorkOrder> WorkOrders { get; set; } = new List<WorkOrder>();
 
+        [BindProperty]
+        public WorkOrder NewWorkOrder { get; set; } = new WorkOrder();
+
+        [BindProperty]
+        public WorkOrder EditWorkOrder { get; set; } = new WorkOrder();
+
+        [BindProperty]
+        public string filterByName { get; set; }
+
+        [BindProperty]
+        public string filterByStatus { get; set; }
+
+        [BindProperty]
+        public DateTime filterByStartDate { get; set; }
+
+        [BindProperty]
+        public DateTime filterByEndDate { get; set; }
+
+        [BindProperty]
+        public string filterByResponsible { get; set; }
+
+        [BindProperty]
+        public int DeleteWorkOrderId { get; set; }
+
+        [BindProperty]
+        public string ErrorMessage { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            WorkOrders = await WorkOrderManager.GetWorkOrdersAsync(filterByName, filterByStartDate, filterByEndDate, filterByResponsible,
+                "Name", filterByStatus, true);
+            if(WorkOrders != null)
+            {
+                return Page();
+            }
+            return RedirectToPage("/Error", new { ErrorMessage = "Не удалось получить данные из базы данных." });
         }
     }
 }
